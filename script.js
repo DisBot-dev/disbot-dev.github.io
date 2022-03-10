e244614af74543c6ef55e129a161707bbc5ec32b
CodeElevator <98259474+CodeElevator@users.noreply.github.com>
2022-03-10 13:24:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typed', {
-    strings: ["/help", "@DisBot", "Professional Discord Bot", "Best work tool", "Easy To Use"],
+    strings: ["/help", "@DisBot", "Professional Discord Bot", "Best work tool", "Easy To Use", "Slash commands only"],
     typeSpeed: 80,
     backSpeed: 70,
     loop: true,
